f0cff13df8817a9535fe568064896e2247594e2f
shlomimaalumi <shlomom.maalumi@mail.huji.ac.il>
2023-08-01 14:51:40 +0300

    Simulates the intersection of 'The Sixteenth' with 'Moshe Dayan'

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ from scheduler import Scheduler
 from intersection import Intersection
 from traffic_light import TrafficLight
 from enums import *
-if __name__ == '__main__':
-    t1 = TrafficLight([])  # Create a new instance of TrafficLight
-    t2 = TrafficLight([])
-    t3 = TrafficLight([])
-    t4 = TrafficLight([])
-    t5 = TrafficLight([])
-    t6 = TrafficLight([])
+from passage import Passage
 
-    scheduler = Scheduler(SchedulerType.SINGLE_RANDOM_SCHEDULER)
-    intersection = Intersection([t1, t2, t3, t4, t5, t6])
-    run = ControlLoop(intersection, scheduler)
+if __name__ == '__main__':
+    p1 = Passage((14, 8), (16, 10))  # from hashisha asar right to neve yaakov
+    p2 = Passage((14, 8), (10, 14))  # from hashisha asar left to moshe dayan
+    p3 = Passage((16, 13), (12, 8))  # from neve yaakov left to hashisa asar
+    p4 = Passage((16, 14), (10, 14))  # from neve yaakov straight to moshe dayan
+    p5 = Passage((10, 10), (12, 8))  # from moshe dayan right to hashisa asar
+    p6 = Passage((10, 10), (16, 10))  # from moshe dayan straight to neve yaakov
+
+    t1 = TrafficLight([p1, p2])
+    t2 = TrafficLight([p5,p6])
+    t3 = TrafficLight([p4])
+    t4 = TrafficLight([p3])
+
+    scheduler = Scheduler(SchedulerType.RANDOM_SCHEDULER)
+    intersection = Intersection([t1, t2, t3, t4])
+    run = ControlLoop(intersection, scheduler, 2.5)
     run.run()
